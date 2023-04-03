8e06cac72f6e73acfd769e22546ac9def2acb690
jjaha99 <jainiljaha@gmail.com>
2023-04-03 15:37:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
 const mysql = require('mysql');
-const { promisify } = require('util');
-const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const upload = multer({ dest: 'upload/' });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const port = 3000;
 
@@ -25,77 +34,62 @@ connection.connect();
 
 // Helper function to segment video
 function segmentVideo(inputFile, outputPrefix, duration, callback) {
-  ffmpeg(inputFile)
-    .outputOptions([`-segment_time ${duration}`, `-f segment`, `-reset_timestamps 1`])
-    .output(`${outputPrefix}%03d.mp4`)
-    .on('end', () => {
-      callback();
-    })
-    .on('error', (err) => {
-      console.error(err);
-      callback(err);
-    })
-    .run();
-}
+    console.log(`Segmenting video: ${inputFile}`);
+    ffmpeg(inputFile)
+      .outputOptions([
+        '-c copy', // Copies the input codec to the output
+        '-map 0', // Maps all input streams to the output
+        '-f segment', // Sets the output format to 'segment'
+        `-segment_time ${duration}`, // Sets the segment duration
+        '-reset_timestamps 1', // Resets timestamps for each segment
+      ])
+      .output(`${outputPrefix}%03d.mp4`)
+      .on('end', () => {
+        console.log('Finished segmenting video');
+        callback();
+      })
+      .on('error', (err) => {
+        console.error(err);
+        callback(err);
+      })
+      .run();
+  }
+  
 
 // Route for handling video uploads
 app.post('/upload', upload.single('video_chunk'), (req, res) => {
   const inputFile = req.file.path;
   const outputPrefix = 'public/uploads/segment_';
+  const duration = 10; // Segment duration in seconds
 
-  segmentVideo(inputFile, outputPrefix, 3, async (err) => {
+  segmentVideo(inputFile, outputPrefix, duration, (err) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Error processing video');
+      console.error('Error segmenting video:', err);
+      res.status(500).send('Error segmenting video');
     } else {
-      // Save metadata to MySQL database and send a response
-      const filename = req.file.originalname;
-      const size = req.file.size;
-      const duration = await getVideoDuration(`${outputPrefix}001.mp4`);
-      const sql = 'INSERT INTO videos (filename, size, duration, upload_time) VALUES (?, ?, ?, NOW())';
-      connection.query(sql, [filename, size, duration], (error) => {
-        if (error) {
-          console.error(error);
-          res.status(500).send('Error saving metadata');
-        } else {
-          res.status(200).send('Video uploaded and segmented successfully');
-        }
-      });
+      res.status(200).send('Video uploaded and segmented');
     }
   });
 });
 
 // Route for getting video list
 app.get('/videos', (req, res) => {
-  const sql = 'SELECT * FROM videos ORDER BY upload_time DESC';
+  const sql = 'SELECT * FROM videos';
   connection.query(sql, (error, results) => {
     if (error) {
-      console.error(error);
       res.status(500).send('Error retrieving video list');
     } else {
-      const videos = results.map((row) => {
-        return {
-          id: row.id,
-          filename: row.filename,
-          size: row.size,
-          duration: row.duration,
-          uploadTime: row.upload_time,
-        };
-      });
-      res.json(videos);
+      res.json(results);
     }
   });
 });
 
-// Route for getting video segments
-app.get('/segments/:id', async (req, res) => {
-  const videoId = req.params.id;
-  const sql = 'SELECT filename FROM videos WHERE id = ?';
-  connection.query(sql, [videoId], async (error, results) => {
-    if (error || results.length === 0) {
-      console.error(error);
-      res.status(404).send('Video not found');
-      return;
-    }
-    const filename = results[0].filename;
-    const segments = await getVideoSegments(`public/uploads
+// Route for serving video segments
+app.get('/uploads/:filename', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/uploads', req.params.filename));
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
